db1c0df857c599af32d15a0c49422d75bbc4d85f
Yash Goyal <yashgo0018@gmail.com>
2023-12-03 16:01:45 +0530

    added polygon network

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    polygonMumbai: {
+      url: process.env.POLYGON_MUMBAI_RPC,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
 };
 
 export default config;
