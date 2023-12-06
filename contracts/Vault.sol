81bf3300f883474fd35bc69b8e2e816cc564f26d
Yash Goyal <yashgo0018@gmail.com>
2023-12-06 10:20:19 +0530

    added chainlink data feeds and uniswap swap in vault contract

--- contracts/Vault.sol
+++ contracts/Vault.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.20;
 
 import "./VaultShareToken.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./FundManager.sol";
+import "./interfaces/ExtendedIERC20.sol";
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
+import "@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol";
 
 contract Vault {
     VaultShareToken public shareToken;
-    IERC20 public stableToken;
+    ExtendedIERC20 public stableToken;
+    FundManager public fundManager;
+    IUniswapV2Router02 public uniswapRouter;
 
     address[] public supportedTokens;
 
@@ -15,10 +21,16 @@ contract Vault {
     event AddSupportedToken(address indexed token);
     event RemoveSupportedToken(address indexed token);
 
-    constructor(IERC20 _stableToken) {
+    constructor(
+        ExtendedIERC20 _stableToken,
+        FundManager _fundManager,
+        address _uniswapRouter
+    ) {
+        fundManager = _fundManager;
         stableToken = _stableToken;
         shareToken = new VaultShareToken();
         supportedTokens.push(address(stableToken));
+        uniswapRouter = IUniswapV2Router02(_uniswapRouter);
 
         emit AddSupportedToken(address(stableToken));
     }
@@ -67,7 +79,7 @@ contract Vault {
         uint256 totalSupportedTokens = supportedTokens.length;
         for (uint256 i = 0; i < totalSupportedTokens; i++) {
             address tokenAddress = supportedTokens[i];
-            IERC20 token = IERC20(tokenAddress);
+            ExtendedIERC20 token = ExtendedIERC20(tokenAddress);
             uint256 share = (token.balanceOf(address(this)) * amount) /
                 totalSupply;
             token.transfer(msg.sender, share);
@@ -86,12 +98,76 @@ contract Vault {
     }
 
     function calculateTotalValue() public view returns (uint256 total) {
-        total = IERC20(supportedTokens[0]).balanceOf(address(this));
+        total = stableToken.balanceOf(address(this));
 
         uint256 totalSupportedTokens = supportedTokens.length;
         for (uint256 i = 1; i < totalSupportedTokens; i++) {
             address tokenAddress = supportedTokens[i];
-            total += IERC20(tokenAddress).balanceOf(address(this)) * 1;
+            uint256 tokenBalance = ExtendedIERC20(tokenAddress).balanceOf(
+                address(this)
+            );
+            total += getTokenValue(tokenAddress, tokenBalance);
         }
+    }
+
+    function getTokenValue(
+        address tokenAddress,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 id = fundManager.tokenAddressToId(tokenAddress);
+        if (id == 0) revert("Token not found");
+
+        (, , , address chainlinkDataFeedAddress, ) = fundManager.tokens(id);
+
+        AggregatorV3Interface chainlinkDataFeed = AggregatorV3Interface(
+            chainlinkDataFeedAddress
+        );
+        (, int256 price, , , ) = chainlinkDataFeed.latestRoundData();
+
+        uint8 tokenDecimals = ExtendedIERC20(tokenAddress).decimals();
+        uint256 tokenUnit = 10 ** tokenDecimals;
+        uint8 feedDecimals = chainlinkDataFeed.decimals();
+        uint256 feedUnit = 10 ** feedDecimals;
+        uint8 stableTokenDecimals = stableToken.decimals();
+        uint256 stableTokenUnit = 10 ** stableTokenDecimals;
+
+        return
+            (stableTokenUnit * amount * uint256(price)) /
+            (tokenUnit * feedUnit);
+    }
+
+    function rebalance() public {
+        uint256 totalSupportedTokens = supportedTokens.length;
+
+        // sell all the tokens in favor of the stable token
+        for (uint256 i = 1; i < totalSupportedTokens; i++) {
+            address tokenAddress = supportedTokens[i];
+            uint256 tokenBalance = ExtendedIERC20(tokenAddress).balanceOf(
+                address(this)
+            );
+            _swapTokens(tokenAddress, address(stableToken), tokenBalance);
+        }
+
+        // TODO: buy the tokens in the propostion given by the fund manager contract
+    }
+
+    function _swapTokens(
+        address _tokenIn,
+        address _tokenOut,
+        uint256 _amount
+    ) internal {
+        ExtendedIERC20(_tokenIn).approve(address(uniswapRouter), _amount);
+
+        address[] memory path = new address[](2);
+        path[0] = _tokenIn;
+        path[1] = _tokenOut;
+
+        uniswapRouter.swapExactTokensForTokens(
+            _amount,
+            0,
+            path,
+            address(this),
+            block.timestamp + 300
+        );
     }
 }
