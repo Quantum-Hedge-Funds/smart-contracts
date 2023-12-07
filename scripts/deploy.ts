b77b481f8af89d3a6df0a1579f3732431dcf3bc8
Yash Goyal <yashgo0018@gmail.com>
2023-12-07 18:19:18 +0530

    updated the deployment config for polygon mainnet

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,13 @@
 import { viem } from "hardhat";
 
-const chainlinkFunctionsRouter = "0x6E2dc0F9DB014aE19888F539E59285D2Ea04244C";
-const uniswapV2RouterAddress = "0x";
-const stableTokenAddress = "0x";
+const chainlinkFunctionsRouter = "0xdc2AAF042Aeff2E68B3e8E33F19e4B9fA7C73F10";
+const uniswapV2RouterAddress = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff";
+const stableTokenAddress = "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174";
+
+const donId =
+  "0x66756e2d706f6c79676f6e2d6d61696e6e65742d310000000000000000000000";
+const donHostedSecretsVersion = 0n;
+const donHostedSecretsSlotID = 0;
 
 async function main() {
   const fundManager = await viem.deployContract("FundManager", [
@@ -18,6 +23,12 @@ async function main() {
   ]);
 
   console.log(`Vault Contract is deployed to ${vault.address}`);
+
+  await fundManager.write.setDONConfig([
+    donHostedSecretsSlotID,
+    donHostedSecretsVersion,
+    donId,
+  ]);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
